e9a7eba42c5ac8c5b554678fb1873aebe6f0f158
“zjjm” <512425998@qq.comgit config --global user.name zjjm  git config --global user.email 512425998@qq.com>
2023-03-12 00:02:06 +0800

    描述信息

---
+++ test2.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class test1 {
+    public static void main(String[] args) {
+
+
+    }
+    @Test
+    public void test1(){
+        System.out.println("1");
+    }
+}
